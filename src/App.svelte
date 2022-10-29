a6b3ed398967a50dfbdadf2f2042f968f0fb4c2c
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-10-29 15:24:23 -0500

    create Header and Footer components

--- src/App.svelte
+++ src/App.svelte
@@ -1,68 +1,17 @@
 <script>
-	import Modal from './Modal.svelte';
-
-	// export let name; // export designates that name is being set from a prop outside of the component (given a prop in main.js)
-
-	// svelte requires id when using loops
-	let people = [
-		{name: 'Maggie', beltColor: 'black', age: 36, id: 1},
-		{name: 'Patrick', beltColor: 'blue', age: 36, id: 2},
-		{name: 'Murph', beltColor: 'pink', age: 5, id: 3}
-	];
-
-	const handleDelete = (id) => {
-		// delete person from people
-		// runs through the people array
-		// if that id is NOT equal to the id clicked it lets it be
-		// if that id IS equal to the id clicked it filters it out of the array
-		people = people.filter((person) => person.id != id)
-	}
-
-	let num = 2;
+	import Header from './components/Header.svelte';
+	import Footer from './components/Footer.svelte';
 </script>
 
-{#if num > 20}
-	<p>Greater than 20</p>
-{:else if num > 5}
-	<p>Greater than 5</p>
-{:else}
-	<p>Not greater than 5</p>
-{/if}
-
-<Modal />
+<Header />
 <main>
-	{#each people as person (person.id)}
-		<div>
-			<h4>{person.name}</h4>
-			{#if person.beltColor === 'black'}
-				<p>MASTER NINJA</p>
-			{/if}
-			<p>{person.age} years old, {person.beltColor} belt</p>
-			<button on:click={() => {handleDelete(person.id)}}>Delete</button>
-		</div>
-	{:else}
-		<p>No people to show...</p>
-	{/each}
+	<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
 </main>
+<Footer />
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	main{
+		max-width: 960px;
+		margin: 40px auto;
 	}
 </style>
---
+++ src/components/Footer.svelte
@@ -0,0 +1,19 @@
+<footer>
+    <div class="copyright">
+        copyright 2020 Murph 
+    </div>
+</footer>
+
+<style>
+    footer {
+        padding: 40px;
+        text-align: center;
+    }
+    .copyright {
+        color: #aaa;
+        font-size: 14px;
+        display: inline-block;
+        padding: 20px;
+        border-top: 1px solid #ddd;
+    }
+</style>
