84116eb743008e8b16e9568f3ebf27375a82354e
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-10-29 16:44:23 -0500

    create form validation

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -3,9 +3,41 @@
 
     // instead of creating a separate variable for each input create an object with properties for each input
     let fields = { question: '', answerA: '', answerB: '' };
+    let errors = { question: '', answerA: '', answerB: '' };
+    let valid = false;
 
     const handleSubmit = () => {
         console.log(fields);
+        valid = true;
+        // validate question
+        if (fields.question.trim().length < 5) {
+            valid = false;
+            errors.question = 'Question must be at least 5 characters long';
+        }
+        else {
+            errors.question = '';
+        }
+        // validate answer A
+        if (fields.answerA.trim().length < 1) {
+            valid = false;
+            errors.answerA = 'Answer A must be at least 1 character long';
+        }
+        else {
+            errors.answerA = '';
+        }
+        // validate answer B
+        if (fields.answerB.trim().length < 1) {
+            valid = false;
+            errors.answerB = 'Question must be at least 1 character long';
+        }
+        else {
+            errors.answerB = '';
+        }
+
+        // add poll
+        if (valid) {
+            console.log('valid', fields);
+        }
     }
 </script>
 
@@ -13,16 +45,19 @@
     <div class="form-field">
         <label for="question">Poll Question:</label>
         <input type="text" id="question" bind:value={fields.question}>
+        <div class="error">{errors.question}</div>
     </div>
 
     <div class="form-field">
         <label for="answer-a">Answer A:</label>
         <input type="text" id="answer-a" bind:value={fields.answerA}>
+        <div class="error">{errors.answerA}</div>
     </div>
 
     <div class="form-field">
         <label for="answer-b">Answer B:</label>
         <input type="text" id="answer-b" bind:value={fields.answerB}>
+        <div class="error">{errors.answerB}</div>
     </div>
     <!-- whatever is between button tag will show up in the "slot" in Button component -->
     <Button type="secondary" flat={true}>Add Poll</Button>
@@ -44,5 +79,10 @@
     label {
         margin: 10px auto;
         text-align: left;
+    }
+    .error {
+        font-weight: bold;
+        font-size: 12px;
+        color: #d91b42;
     }
 </style>
