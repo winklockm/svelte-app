5ccfda73701feb9a764075e4474ae7d04ce25d05
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-10-29 15:59:06 -0500

    add reusable tab component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,27 @@
 <script>
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
+	import Tabs from './shared/Tabs.svelte';
+
+	// tabs
+	let items = ['Current Polls', 'Add New Poll'];
+	let activeItem = 'Current Polls';
+	const tabChange = (e) => {
+		activeItem = e.detail;  //e.detail is what we sent along in Tabs.svelte
+	}
 </script>
 
 <Header />
 <main>
-	<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+	<!-- pass activeItem and items in as props to Tabs component -->
+	<!-- Tabs is listening for tabChange event -->
+	<Tabs {activeItem} {items} on:tabChange={tabChange}/>
+	<!-- show different components based on activeItem selected -->
+	{#if activeItem === 'Current Polls'}
+		<p>Poll list component goes here</p>
+	{:else if activeItem === 'Add New Poll'}
+		<p>New poll form component goes here</p>
+	{/if}
 </main>
 <Footer />
 
---
+++ src/shared/Tabs.svelte
@@ -0,0 +1,44 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+    // use dispatch function to dispatch an event whenever a user clicks on an li tag
+    const dispatch = createEventDispatcher();
+
+    // accept the props passed in from app by adding export in front of variables
+    export let items;
+    export let activeItem;
+</script>
+
+<div class="tabs">
+    <ul>
+        {#each items as item}
+            <!-- when an li is clicked dispatch a tabChange event and send along the item that was clicked -->
+            <li on:click={() => dispatch('tabChange', item)}>
+                <!-- if the item = activeItem apply class of active -->
+                <div class:active={item === activeItem}>{item}</div>
+            </li>
+        {/each}
+    </ul>
+</div>
+
+<style>
+    .tabs {
+        margin-bottom: 40px;
+    }
+    ul {
+        display: flex;
+        justify-content: center;
+        padding: 0;
+        list-style-type: none;
+    }
+    li {
+        margin: 0 16px;
+        font-size: 18px;
+        color: #555;
+        cursor: pointer;
+    }
+    .active {
+        color: #d91b42;
+        border-bottom: 2px solid #d91b42;
+        padding-bottom: 8px;
+    }
+</style>
