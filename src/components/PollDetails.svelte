b1a73ec3691db066f974bf7e6ed2305083189305
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-10-30 15:58:13 -0500

    add colorful percentage voting bars

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -6,6 +6,9 @@
 
     // reactive values
     $: totalVotes = poll.votesA + poll.votesB;
+    // percentage values for voting bar
+    $: percentA = Math.floor(100 / totalVotes * poll.votesA);
+    $: percentB = Math.floor(100 / totalVotes * poll.votesB);
 
     // handling votes
     const handleVote = (option, id) => {
@@ -19,11 +22,11 @@
         <h3>{poll.question}</h3>
         <p>Total Votes: {totalVotes}</p>
         <div class="answer" on:click={() => handleVote('a', poll.id)}>
-            <div class="percent percent-a"></div>
+            <div class="percent percent-a" style="width: {percentA}%"></div>
             <span>{poll.answerA} ({poll.votesA})</span>
         </div>
         <div class="answer" on:click={() => handleVote('b', poll.id)}>
-            <div class="percent percent-b"></div>
+            <div class="percent percent-b" style="width: {percentB}%"></div>
             <span>{poll.answerB} ({poll.votesB})</span>
         </div>
     </div>
@@ -52,6 +55,19 @@
     span {
         display: inline-block;
         padding: 10px 20px;
+    }
+    .percent {
+        height: 100%;
+        position: absolute;
+        box-sizing: border-box;
+    }
+    .percent-a {
+        border-left: 4px solid #d91b42;
+        background: rgba(217,27,66,0.2);
+    }
+    .percent-b {
+        border-left: 4px solid #45c495;
+        background: rgba(69,196,150,0.2);
     }
 
 </style>
